24271e07614fc6738a6bf332dd2c80b400159562
luanlel <luan.l.silva6@ba.estudante.senai.br>
2025-09-25 09:58:24 -0300

    .

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC8MASTlWG-rYMdAVQGfDm5zP-IGtdp42Q",
   authDomain: "lanchonetetestedesistemas.firebaseapp.com",
@@ -13,7 +11,6 @@ const firebaseConfig = {
   appId: "1:983784344399:web:bd76034c939f3dffcc533a"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
